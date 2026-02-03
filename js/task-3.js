cca2d911d36a20aa6e642934ba737327009d6ad6
Roman <romchuck.kuzyshyn@gmail.com>
2026-02-03 17:22:38 +0200

    start hw8

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const inputName = document.querySelector('#name-input');
-const output = document.querySelector('#name-output');
-
-inputName.addEventListener('input', event => {
-  const value = event.target.value.trim();
-
-  output.textContent = value === '' ? 'Anonymous' : value;
-});
